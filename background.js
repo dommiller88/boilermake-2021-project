e503e74d881cc22a7c20504c86a4bcc163d09b25
dommiller88 <dommiller88@gmail.com>
2021-01-23 15:40:53 -0500

    added blocking

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
-chrome.tabs.onActivated.addListener(tab => {
-    console.log(tab);
+const url = 'https://www.desmos.com/*'
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.url != null && changeInfo.url.match(url)) {
+        chrome.tabs.update(null, {active: true, url:"http://boilermake-2021.s3-website-us-east-1.amazonaws.com/"}, function(tab){});
+    }
 })
+
+function block(tab) {
+
+}
 
 //chrome.tabs.executeScript(null, {file: "./foreground.js"}, () => console.log("Here I am"));
 console.log("from backend");
