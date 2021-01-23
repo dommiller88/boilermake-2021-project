bfa847bdb31b5f74e941866a60fb27d6cfe61a58
dommiller88 <dommiller88@gmail.com>
2021-01-23 16:53:25 -0500

    blocking works with storage now

--- background.js
+++ background.js
@@ -1,14 +1,26 @@
 const url = 'https://www.desmos.com/*'
+const url2 = 'https://www.gradescope.com/*'
+chrome.storage.local.set({
+    "sites": [url, url2]
+});
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.url != null && changeInfo.url.match(url)) {
-        chrome.tabs.update(null, {active: true, url:"http://boilermake-2021.s3-website-us-east-1.amazonaws.com/"}, function(tab){});
-    }
+    chrome.storage.local.get("sites", sites => {
+        console.log(sites.sites.length);
+        console.log(changeInfo.url);
+        for (i = 0; i < sites.sites.length; i++) {
+            console.log(sites.sites[i]);
+            if (changeInfo.url != null && changeInfo.url.match(sites.sites[i])) {
+                console.log("match")
+                chrome.tabs.update(null, {active: true, url:"http://boilermake-2021.s3-website-us-east-1.amazonaws.com/"}, function(tab){});
+            }
+        }
+    });
+    
+    
 })
 
-function block(tab) {
 
-}
 
 //chrome.tabs.executeScript(null, {file: "./foreground.js"}, () => console.log("Here I am"));
 console.log("from backend");
